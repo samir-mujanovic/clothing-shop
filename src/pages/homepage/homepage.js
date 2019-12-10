53d20ce17648fcb49584fc2265b5f800d237e57b
Samir <42714951+sameerrM@users.noreply.github.com>
2019-12-10 23:18:08 +0100

    added featured products on homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,17 +1,44 @@
 import React from "react";
 
-import Directory from "../../components/Directory/Directory";
-import FeaturedPrevew from "../../components/FeaturedPreview/FeaturedPrevew";
+import SHOP_DATA from "../shop/shop.data";
 
-const homepage = () => {
-  return (
-    <section className="home-page">
-      <div className="container-fluid p-0">
-        <Directory />
-      </div>
-      <FeaturedPrevew />
-    </section>
-  );
-};
+import Directory from "../../components/Directory/Directory";
+import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
+import HeadingTitle from "../../components/HeadingTitle/HeadingTitle";
+
+import "./homepage.styles.scss";
+
+class homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <section className="home-page">
+        <div className="container-fluid p-0">
+          <Directory />
+        </div>
+        <div className="featured-section">
+          <HeadingTitle
+            title="Featured Products"
+            subtitle="Freegan actually brunch, lumbersexual fanny pack aesthetic tbh tofu
+        cardigan yr quinoa hot chicken bushwick normcore."
+          />
+          <div className="container">
+            {collections
+              .filter(item => item.title === "Womens" || item.title === "Mens")
+              .map(({ id, ...otherCollectionProps }) => {
+                return <CollectionPreview key={id} {...otherCollectionProps} />;
+              })}
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default homepage;
