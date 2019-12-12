59ec88b5d43ea23e54db92d20a9c0c2b193dbc26
Samir <42714951+sameerrM@users.noreply.github.com>
2019-12-12 23:36:46 +0100

    correction in heading,navbar,checkout page

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -4,6 +4,7 @@ import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/CheckoutItem/CheckoutItem";
 import HeadingBlock from "../../components/HeadingBlock/HeadingBlock";
+import CustomButon from "../../components/CustomButton/CustomButton";
 
 import {
   selectCartItems,
@@ -39,7 +40,20 @@ const CheckoutPage = ({ cartItems, total }) => {
               ))}
             </table>
           </div>
-          <div className="total">
+          <div className="cuopon-form mt-4">
+            <form className="form-inline">
+              <div className="form-group mr-sm-3 mb-2">
+                <label className="sr-only">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Coupon code"
+                />
+              </div>
+              <CustomButon title="Apply Coupon" />
+            </form>
+          </div>
+          <div className="total mt-5">
             <span>Total: ${total}</span>
           </div>
         </div>
